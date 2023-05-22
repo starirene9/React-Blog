ed98f8f2c6e36650fb662d7e9ba091f226431da9
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-05-22 10:40:33 +0900

    # 1. 컴포넌트 기초

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import SayHello from './SayHello';
+
 
 function App() {
+
+  const looping = () => {
+    const helloList = [];
+    for (let i = 0; i < 5; i++) {
+      helloList.push(<SayHello />);
+    }
+    return helloList;
+  };
+
+  // jsx문법에서는 스크립트를 코드를 직접 사용 불가능
+  // {} 안에서 함수호출문이나 변수참조를 할 수 있음.
   return (
-    <h1>리액트 초보입니다~~ ㅋ</h1>
+    <>
+      { looping() }
+    </>
   );
 }
 
---
+++ src/SayHello.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// 컴포넌트
+function SayHello() {
+    return (
+        <div>
+            <p>안녕</p>
+            <span>메롱</span>
+        </div>
+    );
+}
+
+export default SayHello;
