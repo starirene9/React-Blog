889dc5aa703bd4ce90fa5793cd3bfca7dc77c4aa
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-06-07 01:43:39 +0900

    부트스트랩이용 컴포넌트와 map함수, import & export studying

---
+++ shop/src/data.js
@@ -0,0 +1,33 @@
+let data = [
+    {
+        id : 0,
+        title : "영어면접 뽀개기",
+        content : "외항사 기출 영어 면접 깨알팁",
+        price : 120000,
+        src : "/svc.jpg",
+        alt : "승무원사진"
+    },
+
+    {
+        id : 1,
+        title : "호텔&항공 영어회화",
+        content : "서비스업계 영어회화 실력 다지기",
+        price : 110000,
+        src : "/sky.jpg",
+        alt : "비행기사진"
+    },
+
+    {
+        id : 2,
+        title : "기내 방송",
+        content : "한국어 & 영어 기내 방송과 발음 교정",
+        price : 130000,
+        src : "/interphone.jpeg",
+        alt : "인터폰사진"
+    }
+]
+
+
+// array[] 배열 안에 object{} 들어가있는 것임
+// let b = {name : 'bitna', age: 30} 사용시 b.name b.age 이렇게 사용함
+export default data;
